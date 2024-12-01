5633d0cd6513a88e1412984a202cd2506f9f1a6b
Mustafa Tariq <mt2032303@gmail.com>
2024-12-01 16:16:20 +0500

    first commit

--- src/components/DoctorDetails.jsx
+++ src/components/DoctorDetails.jsx
@@ -5,79 +5,6 @@ import { collection, addDoc } from "firebase/firestore";
 import { db } from "../App";
 
 
-const doctorsData = [
-  {
-    name: "Dr. Fatima Bukharie",
-    role: "Consultant Psychiatrist",
-    experience: "9+ years in Psychiatry",
-    education: "MBBS, FCPS (Psychiatry)",
-    availability: {
-      Monday: { start: "14:00", end: "20:00" },
-      Tuesday: { start: "14:00", end: "20:00" },
-      Wednesday: { start: "14:00", end: "20:00" },
-      Thursday: { start: "14:00", end: "20:00" },
-      Friday: { start: "14:00", end: "20:00" },
-      Saturday: { start: "12:00", end: "20:00" },
-    },
-  },
-  {
-    name: "Dr Nayab Iftikhar",
-    role: "Speech and language pathologist",
-    experience: "12 plus years experience",
-    education: "PhD. SLP , from Ifugao states university philippine.",
-    availability: {
-      Monday: { start: "14:00", end: "20:00" },
-      Tuesday: { start: "14:00", end: "20:00" },
-      Wednesday: { start: "14:00", end: "20:00" },
-      Thursday: { start: "14:00", end: "20:00" },
-      Friday: { start: "14:00", end: "20:00" },
-      Saturday: { start: "12:00", end: "20:00" },
-    },
-  },
-  {
-    name: "Dr maahin rizwan",
-    role: "Consultant psychiatrist",
-    experience: "8+ years in Psychiatry",
-    education: "MBBS, FCPS (Psychiatrist)",
-    availability: {
-      Monday: { start: "14:00", end: "20:00" },
-      Tuesday: { start: "14:00", end: "20:00" },
-      Wednesday: { start: "14:00", end: "20:00" },
-      Thursday: { start: "14:00", end: "20:00" },
-      Friday: { start: "14:00", end: "20:00" },
-      Saturday: { start: "12:00", end: "20:00" },
-    },
-  },
-  {
-    name: "Dr Hira Liaqat",
-    role: "Focusing Oriented Therapy (FOT)",
-    experience:
-      "8+ years of experience as am academic and clinical psychologist",
-    education: "BSc Clinical psyche , MS clinical Psyche NUST , PhD scholar",
-    availability: {
-      Monday: { start: "14:00", end: "20:00" },
-      Tuesday: { start: "14:00", end: "20:00" },
-      Wednesday: { start: "14:00", end: "20:00" },
-      Thursday: { start: "14:00", end: "20:00" },
-      Friday: { start: "14:00", end: "20:00" },
-      Saturday: { start: "12:00", end: "20:00" },
-    },
-  },
-];
-
-const addDoctors = async () => {
-  try {
-    for (const doctorData of doctorsData) {
-      const docRef = await addDoc(collection(db, "doctors"), doctorData);
-      console.log("Doctor added with ID: ", docRef.id);
-    }
-    console.log("All doctors added successfully!");
-  } catch (e) {
-    console.error("Error adding doctors: ", e);
-  }
-};
-
-
 const DoctorDetails = () => {
   let allDays = [
     "Monday",
@@ -332,10 +259,6 @@ const DoctorDetails = () => {
           Update Information
         </button>
       </form>
-
-      <button type="submit" onClick={addDoctors}>
-        Send user data
-      </button>
     </div>
   );
 };
